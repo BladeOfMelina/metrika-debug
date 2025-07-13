a36d7824820dee140602546a72ff9059eacdd138
BladeOfMelina <returnkaito@mail.ru>
2025-07-13 23:44:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("SotE");
+document.addEventListener("DOMContentLoaded", () => {
+  const title = document.getElementById("title");
+  const desc = document.getElementById("description");
+  const btn = document.getElementById("revealBtn");
+
+  btn.addEventListener("click", () => {
+    title.textContent = "Судьба неизбежна";
+    desc.innerHTML = "Ты сделал шаг в мир, где история пишет кровь. <br> И лишь тьма знает конец.";
+    btn.style.display = "none";
+  });
+
+  console.log("SotE: Script Loaded");
+});
