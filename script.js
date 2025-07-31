dc8c184f10d2dbbbce0decc4c0abdc1d11273fce
BladeOfMelina <returnkaito@mail.ru>
2025-07-31 14:03:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const title = document.getElementById("title");
   const desc = document.getElementById("description");
   const btn = document.getElementById("revealBtn");
+  const form = document.getElementById("testForm");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    window.location.hash = "submitted" + Math.floor(Math.random() * 1000);
+    console.log("Форма отправлена. Страница не перезагружается. Хэш добавлен.");
+  });
 });
